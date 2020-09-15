ee2e394c632cf36e025504c01d4d388dff02eec6
Admin <glazko17@yandex.ru>
2020-09-15 18:35:58 +0300

    lab1

---
+++ lab1.js
@@ -0,0 +1,12 @@
+let x = prompt('Введите Х:', 0);
+if (isNaN(x)){
+    alert('ERROR! Please press F5 and repeat');
+} else {
+    alert(Math.pow(x, 2)-7*x+10);
+}
+    x = prompt('Введите Х:', 0);
+if (isNaN(x)){
+    alert('ERROR! Please press F5 and repeat')
+} else {
+    alert(Math.pow(x, 2)-8*x+12)
+}
